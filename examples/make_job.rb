6cbe83fbc17da0ec7430aa9efdb2081aa2bff31a
Vizzuality <contact@vizzuality.com>
2011-05-10 18:27:44 +0200

    update example

--- examples/make_job.rb
+++ examples/make_job.rb
@@ -4,7 +4,7 @@ require File.dirname(__FILE__) + "/common"
 @pk = @redis.INCR @keys[:job_pk]
 
 # 2. All R calls from Ruby include primary key as the first argument
-@con.eval("x<-vr.example.1(#{@pk},2,2)")
+@con.eval("x<-vr.example.2(#{@pk},2,2)")
 
 # 3. R call is disconnected as soon as it is made, and the connection is serialised into redis in a set specific to a user_id
 s=@con.detach
