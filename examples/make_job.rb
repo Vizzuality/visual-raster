a2bce46c53a730f944bcc068a03d20cc2e4e816f
Vizzuality <contact@vizzuality.com>
2011-05-10 18:32:39 +0200

    update example

--- examples/make_job.rb
+++ examples/make_job.rb
@@ -4,10 +4,9 @@ require File.dirname(__FILE__) + "/common"
 @pk = @redis.INCR @keys[:job_pk]
 
 # 2. All R calls from Ruby include primary key as the first argument
-@con.void_eval("x<-vr.example.2(#{@pk},2,2)")
+s=@con.void_eval_detach("x<-vr.example.2(#{@pk},2,2)")
 
 # 3. R call is disconnected as soon as it is made, and the connection is serialised into redis in a set specific to a user_id
-s=@con.detach
 connection = Marshal.dump(s)
 @redis.SET(interpolate_job_id(@keys[:user_job], @pk), connection)
 @redis.SADD(@keys[:user_jobs], @pk)
